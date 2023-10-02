1216146f91e150cd926af324d3210a75691348da
Hoang-Ngoc-Anh <hoangngocanh288@gmail.com>
2023-10-02 22:11:30 +0700

    change headless mode to true

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   reporter: 'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://thinking-tester-contact-list.herokuapp.com',
 
